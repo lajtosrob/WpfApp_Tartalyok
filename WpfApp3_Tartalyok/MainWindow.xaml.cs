82e9684828c16e00872276428fe6d702a87b591b
lajtosrob <lajtosrobert@freemail.hu>
2023-04-02 10:26:35 +0200

    Add project files.

---
+++ WpfApp3_Tartalyok/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using SajatOsztalyok;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3_Tartalyok
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Tartaly> tartalyok = new List<Tartaly>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRogzit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnFelvesz_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
