2c4132b3cc0131812092f87c189173fb72788de6
lajtosrob <lajtosrobert@freemail.hu>
2023-04-02 21:05:53 +0200

    ExceptionHangling => btnRogzit, btnTolt

--- WpfApp3_Tartalyok/MainWindow.xaml.cs
+++ WpfApp3_Tartalyok/MainWindow.xaml.cs
@@ -23,14 +23,15 @@ namespace WpfApp3_Tartalyok
     public partial class MainWindow : Window
     {
         List<Tartaly> tartalyok = new List<Tartaly>();
-        string test;
+        string test = null;
         string fajlNev = "tartalyok.csv";
+        double aEl;
+        double bEl;
+        double cEl;
         public MainWindow()
         {
             InitializeComponent();
-
             rdoTeglatest.IsChecked = true;
-
         }
 
         private void btnRogzit_Click(object sender, RoutedEventArgs e)
@@ -44,10 +45,15 @@ namespace WpfApp3_Tartalyok
 
         private void btnFelvesz_Click_1(object sender, RoutedEventArgs e)
         {
-            Tartaly tartaly = new Tartaly(txtNev.Text, Convert.ToInt16(txtAel.Text), Convert.ToInt16(txtBel.Text), Convert.ToInt16(txtCel.Text));
+            if (SzamEllenorzes())
+            {
+            Tartaly tartaly = new Tartaly(txtNev.Text, Convert.ToInt16(aEl), Convert.ToInt16(bEl), Convert.ToInt16(cEl));
             test = rdoTeglatest.IsChecked == true ? "Téglatest" : "Kocka";
             string felvettSor = $"{test}: {tartaly.Terfogat * 1000} cm3 = ({tartaly.Terfogat} liter), töltöttsége: {tartaly.Toltottseg}, méretei: {txtAel.Text} x {txtBel.Text} x {txtCel.Text}";
             lbTartalyok.Items.Add(felvettSor);
+            }
+            
+
         }
 
         private void rdoTeglatest_Checked(object sender, RoutedEventArgs e)
@@ -78,9 +84,27 @@ namespace WpfApp3_Tartalyok
 
         private void btnTolt_Click(object sender, RoutedEventArgs e)
         {
-            Tartaly tartaly = new Tartaly(txtNev.Text, Convert.ToInt16(txtAel), Convert.ToInt16(txtBel), Convert.ToInt16(txtCel));
-            tartaly.Tolt(Convert.ToDouble(btnTolt.Content));
-
+            double tolt;
+            try
+            {
+                tolt = double.Parse(txtTolt.Text);
+                Tartaly tartaly = new Tartaly(txtNev.Text, Convert.ToInt16(txtAel), Convert.ToInt16(txtBel), Convert.ToInt16(txtCel));
+                tartaly.Tolt(tolt);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Nem jó a számformátum!");
+                txtTolt.Text = "1";
+                txtTolt.Focus();
+                return;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Egyéb nem azonosított hiba!");
+                txtTolt.Text = "1";
+                txtTolt.Focus();
+                return;
+            }
 
         }
 
@@ -106,6 +130,58 @@ namespace WpfApp3_Tartalyok
             {
                 txtBel.Text = txtAel.Text;
                 txtCel.Text = txtAel.Text;
+            }
+        }
+
+        private bool SzamEllenorzes()
+        {
+            try
+            {
+                int aEl = Convert.ToInt16(txtAel.Text);
+                int bEl = Convert.ToInt16(txtBel.Text);
+                int cEl = Convert.ToInt16(txtCel.Text);
+                return true;
+
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Nem jó a számformátum!");
+                MilyenTest();
+                return false;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Egyéb nem azonosított hiba!");
+                MilyenTest();
+                return false;
+            }
+
+            
+        }
+
+        private void MilyenTest ()
+        {
+            if (test == "Kocka")
+            {
+                txtNev.Text = "Kocka";
+                txtBel.IsReadOnly = true;
+                txtCel.IsReadOnly = true;
+                txtCel.Background = Brushes.LightGray;
+                txtBel.Background = Brushes.LightGray;
+                txtAel.Text = "10";
+                txtBel.Text = "10";
+                txtCel.Text = "10";
+            }
+            else
+            {
+                txtNev.Text = "Téglatest";
+                txtBel.IsReadOnly = false;
+                txtCel.IsReadOnly = false;
+                txtCel.Background = Brushes.LightSkyBlue;
+                txtBel.Background = Brushes.LightSkyBlue;
+                txtAel.Text = "";
+                txtBel.Text = "";
+                txtCel.Text = "";
             }
         }
     }
